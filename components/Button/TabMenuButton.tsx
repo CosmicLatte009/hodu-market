e57203d1c011f6a6b2788c6f3cd6cc2d1d20b751
CosmicLatte009 <ohhm009@gmail.com>
2023-04-29 22:22:34 +0900

    #1 - Feat: Button 컴포넌트들 생성

---
+++ components/Button/TabMenuButton.tsx
@@ -0,0 +1,20 @@
+import styles from "../../styles/Button/TabMenuButton.module.scss";
+import { ReactNode } from "react";
+
+interface MyProps {
+	children: ReactNode;
+	clicked: string;
+}
+
+const TabMenuButton: React.FC<MyProps> = (props) => {
+	const { children, clicked } = props;
+	const tabMenuClassName = `${styles["tab-menu-btn"]} ${styles[clicked]}`;
+	return (
+		<button className={tabMenuClassName} type="button">
+			{children}
+			<span className={styles.badge}>1</span>
+		</button>
+	);
+};
+
+export default TabMenuButton;
