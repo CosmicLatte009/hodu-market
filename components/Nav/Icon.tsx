dd5e770602ea3b13a0ba3f9a3382f61e66c8e49e
CosmicLatte009 <ohhm009@gmail.com>
2023-05-16 19:49:57 +0900

    Closes #14 - Refactor: Nav 컴포넌트 로그인 유형에 따라 구분

--- components/Nav/Icon.tsx
+++ components/Nav/Icon.tsx
@@ -1,17 +1,18 @@
-import styles from "../../styles/Nav/Icon.module.scss";
+import styles from "../../styles/components/Nav/Icon.module.scss";
 import Image from "next/image";
 
 interface MyProps {
 	children: string;
 	src: string;
 	alt: string;
+	clicked: string;
 }
 
 const Icon: React.FC<MyProps> = (props) => {
-	const { children, src, alt } = props;
-
+	const { children, src, alt, clicked } = props;
+	const iconBtnClassName = `${styles["icon-btn"]} ${styles[clicked]}`;
 	return (
-		<button className={styles["icon-button"]} type="button">
+		<button className={iconBtnClassName} type="button">
 			<Image
 				className={styles["icon-img"]}
 				src={src}
--- components/Nav/IconGroup.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "../../styles/Nav/IconGroup.module.scss";
-
-const IconGroup: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-	return <div className={styles["icon-group"]}>{children}</div>;
-};
-
-export default IconGroup;
