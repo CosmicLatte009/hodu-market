e8b86722ba763fa5ec270309caff0b434499132f
CosmicLatte009 <ohhm009@gmail.com>
2023-05-16 19:49:57 +0900

    Closes #14 - Refactor: Nav 컴포넌트 로그인 유형에 따라 구분

--- components/Nav/TopNavBar.tsx
+++ components/Nav/TopNavBar.tsx
@@ -1,7 +1,34 @@
-import styles from "../../styles/Nav/TopNavBar.module.scss";
+import styles from "../../styles/components/Nav/TopNavBar.module.scss";
+import SearchBar from "./SearchBar";
+import Icon from "./Icon";
 
-const TopNavBar: React.FC<React.PropsWithChildren<{}>> = ({ children }) => {
-	return <header className={styles["header-wrap"]}>{children}</header>;
+const TopNavBar: React.FC = () => {
+	return (
+		<nav className={styles["nav-wrap"]}>
+			<SearchBar />
+			<div className={styles["nav-icon-group"]}>
+				<Icon
+					src="/assets/icon-shopping-cart.svg"
+					alt="장바구니 아이콘"
+					clicked="unclicked"
+				>
+					장바구니
+				</Icon>
+				<Icon src="/assets/icon-user.svg" alt="유저 아이콘" clicked="unclicked">
+					로그인
+				</Icon>
+				{/* <Button size="ms" theme="icon">
+            <Image
+                src="/assets/icon-shopping-bag.svg"
+                alt="쇼핑백 아이콘"
+                width={32}
+                height={32}
+            />
+            판매자 센터
+        </Button> */}
+			</div>
+		</nav>
+	);
 };
 
 export default TopNavBar;
