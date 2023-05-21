d8b6a52398dc651831cc557bcdf3566a2929e01c
CosmicLatte009 <ohhm009@gmail.com>
2023-05-21 19:08:00 +0900

    Feat: make-product 페이지 전체 레이아웃 스타일링

--- pages/make-product.tsx
+++ pages/make-product.tsx
@@ -6,15 +6,17 @@ function MakeProductPage() {
 	return (
 		<>
 			<SellerCenterTopNavBar />
-			<header className={styles.header}>
-				<h1>상품 등록</h1>
-			</header>
 			<main className={styles["main-wrap"]}>
-				<aside className={styles["aside-wrap"]}>
-					<strong>* 상품 등록 주의사항</strong>
-					<div></div>
-				</aside>
-				<MakeProductForm />
+				<header className={styles.header}>
+					<h1>상품 등록</h1>
+				</header>
+				<section className={styles["section-wrap"]}>
+					<aside className={styles["aside-wrap"]}>
+						<strong>*상품 등록 주의사항</strong>
+						<div></div>
+					</aside>
+					<MakeProductForm />
+				</section>
 			</main>
 		</>
 	);
